ce2b6e6ad97d35be2e87c59dabef5fdd443e537a
JakeWharton <jakewharton@gmail.com>
2009-07-17 13:56:08 -0400

    Non-html_tag based iterables are now supported. The .add() method now returns all arguments given.

--- xhtml11.py
+++ xhtml11.py
@@ -147,7 +147,7 @@ class htmlpage(basepage):
             import web
             if not web.is_internetexplorer:
                 print '<?xml version="1.0" encoding="utf-8"?>'
-            
+        
         print '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">'
         print
         

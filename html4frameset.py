a4edbc60128c6c360d0ecd90bcff942cb954e391
JakeWharton <jakewharton@gmail.com>
2009-07-19 16:21:37 -0400

    Added XHTML 1.0 strict and frameset and HTML 4.01 frameset. Closes #3.

---
+++ html4frameset.py
@@ -0,0 +1,45 @@
+__license__ = '''
+This file is part of pyy.
+
+pyy is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+pyy is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General
+Public License along with pyy.  If not, see
+<http://www.gnu.org/licenses/>.
+'''
+
+from html import html_tag, single
+from html4strict import html, head, title, meta, body,
+                        bdo,
+                        div, span,
+                        h1, h2, h3, h4, h5, h6,
+                        address,
+                        em, strong, dfn, code, samp, kdb, var, cite, abbr, acronym,
+                        blockquote, q,
+                        sub, sup,
+                        p,
+                        br,
+                        pre,
+                        ins, _del,
+                        ul, ol, li, dl, dt, dd,
+                        table, caption, thead, tbody, tfoot, colgroup, col, tr, td, th,
+                        a, link, base,
+                        img, object, param, map, area,
+                        style,
+                        tt, i, b, big, small, strike, s, u, hr,
+                        form, input, button, select, optgroup, option, textarea, label, fieldset, legend,
+                        script, noscript,
+                        htmlpage
+
+class frameset(html_tag): valid = ['rows', 'cols', 'onload', 'onunload'] + COMMON_CORE
+class frame   (single):   valid = ['longdesc', 'name', 'src', 'frameborder', 'marginwidth', 'marginheight', 'noresize', 'scrolling'] + COMMON_CORE
+class noframes(html_tag): valid = COMMON
+class iframe  (html_tag): valid = ['longdesc', 'name', 'src', 'frameborder', 'marginwidth', 'marginheight', 'scrolling', 'align', 'height', 'width'] + COMMON_CORE
---
+++ xhtml10frameset.py
@@ -0,0 +1,40 @@
+__license__ = '''
+This file is part of pyy.
+
+pyy is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+pyy is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General
+Public License along with pyy.  If not, see
+<http://www.gnu.org/licenses/>.
+'''
+
+from xhtml10strict import html, head, title, meta, body,
+                          bdo,
+                          div, span,
+                          h1, h2, h3, h4, h5, h6,
+                          address,
+                          em, strong, dfn, code, samp, kdb, var, cite, abbr, acronym,
+                          blockquote, q,
+                          sub, sup,
+                          p,
+                          br,
+                          pre,
+                          ins, _del,
+                          ul, ol, li, dl, dt, dd,
+                          table, caption, thead, tbody, tfoot, colgroup, col, tr, td, th,
+                          a, link, base,
+                          img, object, param, map, area,
+                          style,
+                          tt, i, b, big, small, strike, s, u, hr,
+                          form, input, button, select, optgroup, option, textarea, label, fieldset, legend,
+                          script, noscript,
+                          htmlpage
+from html4frameset import frameset, frame, noframes, iframe

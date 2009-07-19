14fd2d368f7273e0eabc925c048ca7f025365146
JakeWharton <jakewharton@gmail.com>
2009-07-19 02:04:03 -0400

    Relicensed under LGPL.

--- cgi.py
+++ cgi.py
@@ -2,6 +2,24 @@
 #
 # Edit this file very carefully, a bug will crash your site.
 
+__license__ = '''
+This file is part of pyy.
+
+pyy is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+pyy is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General
+Public License along with pyy.  If not, see
+<http://www.gnu.org/licenses/>.
+'''
+
 if __name__ == '__main__':
 
     import os

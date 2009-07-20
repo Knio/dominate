b5bc85e5e5b7c22b935ba466c8385e029f08f763
JakeWharton <jakewharton@gmail.com>
2009-07-20 00:25:44 -0400

    Fix some imports. Prepend underscores to object, map, and input classes so their normal meanings are not overwritten.

--- html4frameset.py
+++ html4frameset.py
@@ -17,27 +17,8 @@ Public License along with pyy.  If not, see
 '''
 
 from html import html_tag, single
-from html4strict import html, head, title, meta, body,
-                        bdo,
-                        div, span,
-                        h1, h2, h3, h4, h5, h6,
-                        address,
-                        em, strong, dfn, code, samp, kdb, var, cite, abbr, acronym,
-                        blockquote, q,
-                        sub, sup,
-                        p,
-                        br,
-                        pre,
-                        ins, _del,
-                        ul, ol, li, dl, dt, dd,
-                        table, caption, thead, tbody, tfoot, colgroup, col, tr, td, th,
-                        a, link, base,
-                        img, object, param, map, area,
-                        style,
-                        tt, i, b, big, small, strike, s, u, hr,
-                        form, input, button, select, optgroup, option, textarea, label, fieldset, legend,
-                        script, noscript,
-                        htmlpage
+from html4strict import *
+
 
 class frameset(html_tag): valid = ['rows', 'cols', 'onload', 'onunload'] + COMMON_CORE
 class frame   (single):   valid = ['longdesc', 'name', 'src', 'frameborder', 'marginwidth', 'marginheight', 'noresize', 'scrolling'] + COMMON_CORE

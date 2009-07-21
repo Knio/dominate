0ceb89ca6f2279e180c80ef7ec5e89e7d65f820a
Knio <theknio@gmail.com>
2009-07-20 19:22:37 -0700

    i dont know

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
 
     author = 'Tom Flanagan and Jake Wharton',
-    lisence = 'LGPL',
+    license = 'LGPL',
     description = 'pyy is a python library for creating (X)HTML pages with the use of objects. This allows you to tightly integrate (X)HTML generation into your backend without the need of using an intermediate templating language.',
     url = 'http://github.com/Knio/pyy/tree/master',
     

ecd3d3e655640e266e10e41acd334c041ad2c18e
Knio <theknio@gmail.com>
2009-07-30 23:57:15 -0600

    begin separating html validation, dom api, and misc. from the base classes

---
+++ pyy/pyy_tag.py
@@ -0,0 +1,195 @@
+__license__ = '''
+This file is part of pyy.
+
+pyy is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+pyy is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General
+Public License along with pyy.  If not, see
+<http://www.gnu.org/licenses/>.
+'''
+
+class pyy_tag(object):   
+    TAB = '  '#'\t'
+    
+    is_single     = False #Tag does not require matching end tag (ex. <hr/>)
+    is_pretty     = True  #Text inside the tag should be left as-is (ex. <pre>)
+    do_inline     = False #Does not insert newlines on all children if True (recursive attribute)
+
+    
+    def __init__(self, *args, **kwargs):
+        self.attributes = {}
+        self.children   = []
+        self.parent     = None
+        
+
+        # special attributes
+        self.do_inline = kwargs.pop('__inline', False)
+        
+        #Add child elements
+        self.add(*args)
+        
+        for attr, value in kwargs.items():
+            attr = self.clean_attribute(attr)
+            self.set_attribute(attr, value)
+
+
+    def set_attribute(self, attr, value):
+        '''
+        Add or update the value of an attribute.
+        '''        
+        self.attributes[attr] = value
+    __setitem__ = set_attribute
+
+
+    def add(self, *args):
+        if self.is_single and args:
+            raise ValueError("<%s> tag can not have child elements." % type(self).__name__)
+        
+        for obj in args:
+            if   isinstance(obj, basestring):
+                self.children.append(obj)
+            elif isinstance(obj, pyy_tag):
+                self.children.append(obj)
+                obj.parent = self                
+            elif hasattr(obj, '__iter__'):
+                for subobj in obj:
+                    self.add(subobj)
+            else:
+                self.children.append(obj)
+        
+        if len(args) > 1:
+            return args
+        elif len(args) == 1:
+            return args[0]
+    
+    def get(self, tag=None, **kwargs):
+        '''
+        Recursively searches children for tags of a certain type with matching attributes.
+        '''
+        #Stupid workaround since we can not use pyy_tag in the method declaration
+        if not tag: tag = pyy_tag
+        
+        results = []
+        for child in self.children:
+            if (isinstance(tag, basestring) and type(child).__name__ == tag) or (not isinstance(tag, basestring) and isinstance(child, tag)):
+                if all(pyy_tag.clean_attribute(attribute) in child.attributes and child.attributes[pyy_tag.clean_attribute(attribute)] == value for attribute, value in kwargs.iteritems()):
+                    #If the child is of correct type and has all attribute and values in kwargs add as a result
+                    results.append(child)
+            if isinstance(child, pyy_tag):
+                #If the child is an pyy_tag extend the search down its children
+                results.extend(child.get(tag, **kwargs))
+        return results
+    
+    
+    def __getitem__(self, attr):
+        '''
+        Returns the stored value of the specified attribute (if it exists).
+        '''
+        try: return self.attributes[attr]
+        except KeyError: raise AttributeError('Attribute "%s" does not exist.' % attr)
+    __getattr__ = __getitem__
+    
+    def __len__(self):
+        '''
+        Number of child elements.
+        '''
+        return len(self.children)
+    
+    def __iter__(self):
+        '''
+        Iterates over child elements.
+        '''
+        return self.children.__iter__()
+    
+    def __contains__(self, item):
+        '''
+        Checks recursively if item is in children tree. Accepts both a string and class.
+        '''
+        return bool(self.get(item))
+    
+    def __iadd__(self, obj):
+        '''
+        Reflexive binary addition simply adds tag as a child.
+        '''
+        self.add(obj)
+        return self
+    
+    def render(self, indent=1, inline=False):
+        '''
+        Returns a well-formatted string representation of the tag and renderings
+        of all its child tags.
+        '''
+        inline = self.do_inline or inline
+        
+        #Workaround for python keywords
+        if type(self).__name__[0] == "_":
+            name = type(self).__name__[1:]
+        else:
+            name = type(self).__name__
+        rendered = '<' + name
+        
+        for attribute, value in self.attributes.items():
+            rendered += ' %s="%s"' % (attribute, escape.escape(str(value), True))
+        
+        if self.is_single:
+            rendered += ' />'
+        else:
+            rendered += '>'
+            rendered += self.render_children(indent, inline)
+            
+            # if there are no children, or only 1 child that is not an html element, do not add tabs and newlines
+            no_children = self.is_pretty and self.children and (not (len(self.children) == 1 and not isinstance(self.children[0], pyy_tag)))
+            
+            if no_children and not inline:
+                rendered += '\n'
+                rendered += pyy_tag.TAB * (indent - 1)
+            rendered += '</'
+            rendered += name
+            rendered += '>'
+        
+        return rendered
+    
+    #String and unicode representations are the same as render()
+    __str__ = __unicode__ = render
+    
+    def render_children(self, indent=1, inline=False):
+        children = ''
+        for child in self.children:
+            if isinstance(child, pyy_tag):
+                if not inline and self.is_pretty:
+                    children += '\n'
+                    children += pyy_tag.TAB * indent
+                children += child.render(indent + 1, inline)
+            else:
+                children += str(child)
+        return children
+    
+    def __repr__(self):
+        name = '%s.%s' % (self.__module__, type(self).__name__)
+        
+        attributes_len = len(self.attributes)
+        attributes = '%s attribute' % attributes_len
+        if attributes_len > 1: attributes += 's'
+        
+        children_len = len(self.children)
+        children = '%s child' % children_len
+        if children_len > 1: children += 'ren'
+        
+        return '<%s: %s, %s>' % (name, attributes, children)
+    
+    @staticmethod
+    def clean_attribute(attribute):
+        #Workaround for python's reserved words
+        if attribute[0] == '_': attribute = attribute[1:]
+        #Workaround for inability to use colon in python keywords
+        return attribute.replace('_', ':').lower()
+
+
---
+++ pyy/utils.py
@@ -0,0 +1,116 @@
+__license__ = '''
+This file is part of pyy.
+
+pyy is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+pyy is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General
+Public License along with pyy.  If not, see
+<http://www.gnu.org/licenses/>.
+'''
+
+from pyy.pyy_tag    import pyy_tag
+
+class include(pyy_tag):
+    '''
+    includes the contents of a file on disk.
+    takes a filename
+    '''
+    def __init__(self, f):
+        fl = file(f, 'rb')
+        self.data = fl.read()
+        fl.close()
+    
+    def render(self, n=1, inline=False):
+        return self.data
+
+class pipe(pyy_tag):
+    '''
+    pipes the output of a program
+    '''
+    def __init__(self, cmd, data='', mode='t'):
+        import os
+        fin, fout = os.popen4(cmd, mode)
+        fin.write(data)
+        fin.close()
+        self.data = fout.read()
+    
+    def render(self, indent=1, inline=False):
+        return self.data
+
+class escape(pyy_tag):
+    '''
+    escapes special characters into their html entities
+    '''
+    @staticmethod
+    def escape(data, quote=False): # stoled from std lib cgi
+        '''
+        Replace special characters "&", "<" and ">" to HTML-safe sequences.
+        If the optional flag quote is true, the quotation mark character (")
+        is also translated.
+        '''
+        data = data.replace("&", "&amp;") # Must be done first!
+        data = data.replace("<", "&lt;")
+        data = data.replace(">", "&gt;")
+        if quote:
+            data = data.replace('"', "&quot;")
+        return data
+    
+    def render(self, indent=1, inline=False):
+        return self.escape(pyy_tag.render_children(self, indent, inline))
+
+_unescape = {'quot' :34,
+             'amp'  :38,
+             'lt'   :60,
+             'gt'   :62,
+             'nbsp' :32,
+             
+             # more here
+             
+             'yuml' :255
+             }
+
+def unescape(data):
+    '''
+    unescapes html entities
+    '''
+    import re
+    cc = re.compile('&(?:(?:#(\d+))|([^;]+));')
+    
+    result = []
+    m = cc.search(data)
+    while m:
+        result.append(data[0:m.start()])
+        d = m.group(1)
+        if d:
+            d = int(d)
+            result.append(d > 255 and unichr(d) or chr(d))
+        else:
+            d = _unescape.get(m.group(2), ord('?'))
+            result.append(d > 255 and unichr(d) or chr(d))
+            
+        data = data[m.end():]
+        m = cc.search(data)
+    
+    result.append(data)
+    return ''.join(result)
+
+class lazy(pyy_tag):
+    '''
+    delays function execution until render
+    '''
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        
+    def render(self, indent=1, inline=False):
+        return self.func(*self.args, **self.kwargs)
+

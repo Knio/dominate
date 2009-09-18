ccdceb26aab82f1a777c5ce6a8a0d155829f02d1
JakeWharton <jakewharton@gmail.com>
2009-09-17 23:04:40 -0400

    Moved resolve into pyy_web and update to new module workings.

--- pyy_web/__init__.py
+++ pyy_web/__init__.py
@@ -1,1 +1,9 @@
 from httpmessage import *
+
+def resolve(urls, request, response):
+  for regex, pageclass in urls:
+    match = re.match(regex, request.uri)
+    if match:
+      request.get.update(match.groupdict())
+      return pageclass(request, response)
+  raise ValueError("URI did not resolve to a class.")

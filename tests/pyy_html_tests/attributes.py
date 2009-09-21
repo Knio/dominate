d39394e0aac76a5efd143c8a25d77ddea97185a0
JakeWharton <jakewharton@gmail.com>
2009-09-21 16:58:14 -0400

    * Started attribute tests from scratch since validation ones no longer apply. * Added missing attribute check to html5 meta tag.

--- tests/pyy_html_tests/attributes.py
+++ tests/pyy_html_tests/attributes.py
@@ -1,21 +1,12 @@
 import unittest
 from pyy_html.html import html, img
 
-#TODO: These no longer work since stuff like this is only checked when
-#      a doctype is assigned to a document. Update them to reflect.
-
 class AttributeTests(unittest.TestCase):
-    def testDefaultBlank(self):
-        self.assertEqual(str(img(src='test.png')), '<img src="test.png" alt="" />')
+    def testAddViaDict(self):
+        i = img()
+        i['src'] = 'test.png'
+        self.assertEqual(str(i), '<img src="test.png" />')
     
-    def testDefault(self):
-        self.assertEqual(str(html()), '<html xmlns="http://www.w3.org/1999/xhtml"></html>')
-    
-    def testInvalid(self):
-        self.assertRaises(AttributeError, html, src='')
-    
-    def testAllowInvalid(self):
-        try:
-            h = html(src='', __invalid=True)
-        except AttributeError:
-            self.fail()
+    def testAddViaKeywordArg(self):
+        i = img(src='test.png')
+        self.assertEqual(str(i), '<img src="test.png" />')

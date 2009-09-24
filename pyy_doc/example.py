8879e7d51a174d85674724a17fbc7e7d6fe9cddc
JakeWharton <jakewharton@gmail.com>
2009-09-23 22:23:10 -0400

    Fixed tag child/attribute access. Cleaned up pyy_doc rendered output a bit and added licenses.

--- pyy_doc/example.py
+++ pyy_doc/example.py
@@ -1,14 +1,32 @@
-import types
+__license__ = '''
+This file is part of pyy.
 
+pyy is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+pyy is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General
+Public License along with pyy.  If not, see
+<http://www.gnu.org/licenses/>.
+'''
+
+import types
 
 from pyy_html       import *
 from pyy_html.html  import *
 from pyy_html.util  import *
 
+
 class pyy_doc(document):
   def __init__(self, module):
     self.module = module
-    document.__init__(self, '%s - docs' % module, None)
+    document.__init__(self, title='%s - docs' % module)
     self.generate()
 
   def generate(self):
@@ -31,14 +49,14 @@ class pyy_doc(document):
     for k, v in mod.__dict__.items():
       if k.startswith('_'): continue
       if type(v) is types.ModuleType:
-        modules.get()[1] += tr(td(a(k, href=k)), td(escape(str(v.__doc__)[:100])))
+        modules.get()[1] += tr(td(a(k, href=k), __inline=True), td(escape(str(v.__doc__).strip()[:100]), __inline=True))
       elif type(v) is type:
-        classes.get()[1] += tr(td(a(k,href=k)), td(escape(str(v.__doc__)[:100])))
+        classes.get()[1] += tr(td(a(k,href=k), __inline=True), td(escape(str(v.__doc__).strip()[:100]), __inline=True))
       elif type(v) is types.FunctionType:
-        functions.get()[1] += tr(td(a(k+'()',href=k)), td(escape(str(v.__doc__)[:100])))
+        functions.get()[1] += tr(td(a(k+'()',href=k), __inline=True), td(escape(str(v.__doc__).strip()[:100]), __inline=True))
       else:
         print k, v
-        constants.get()[1] += tr(td(k), td(escape(repr(v))))   
+        constants.get()[1] += tr(td(k), td(escape(repr(v)), __inline=True))
 
 
 
@@ -49,8 +67,8 @@ def main(handler, req, res, *args):
   res.headers['Content-Type'] = 'text/html'
   res.body = d.render()
   
-  
-  
-  
+
+
+
 
 
--- pyy_html/pyy_tag.py
+++ pyy_html/pyy_tag.py
@@ -42,10 +42,10 @@ class pyy_tag(object):
         '''
         Add or update the value of an attribute.
         '''
-        if isinstance(key, basestring):
+        if isinstance(key, int):
             self.children[key] = value
-        elif isinstance(key, string):
-            self.attributes[attr] = value
+        elif isinstance(key, basestring):
+            self.attributes[key] = value
         else:
             raise TypeError('Only integer and string types are valid for assigning child tags and attributes, respectively.')
     __setitem__ = set_attribute

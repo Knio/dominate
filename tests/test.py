64d6f592db9dcc91d1c8b4ed0b972e303b7a26de
JakeWharton <jakewharton@gmail.com>
2009-09-24 13:51:33 -0400

    Add check for valid module.

--- tests/test.py
+++ tests/test.py
@@ -26,6 +26,7 @@ sys.path.insert(0, os.path.split(os.path.dirname(os.path.abspath(__file__)))[0])
 from unittest import defaultTestLoader, TestSuite, TextTestRunner
 import pyy_cgi_tests, pyy_doc_tests, pyy_html_tests, pyy_httpserver_tests, pyy_web_tests
 
+#Valid modules to test
 VALID = {'pyy_cgi'       : 1,
          'pyy_doc'       : 2,
          'pyy_html'      : 4,
@@ -33,10 +34,17 @@ VALID = {'pyy_cgi'       : 1,
          'pyy_web'       : 16,
          'all'           : 31}
 
-tests = VALID['all'] if not len(sys.argv[1:]) else 0
-for arg in sys.argv[1:]:
-  tests |= VALID[arg]
+#Default to all modules unless any arguments were passed in
+tests = 0 if len(sys.argv[1:]) else VALID['all']
 
+#Load modules from arguments
+for arg in sys.argv[1:]:
+  if arg not in VALID:
+    print 'INVALID MODULE: %s.' % arg
+  else:
+    tests |= VALID[arg]
+
+#Assemble individual module tests into array
 suites = []
 if tests & VALID['pyy_cgi']:        suites += defaultTestLoader.loadTestsFromModule(pyy_cgi_tests)
 if tests & VALID['pyy_doc']:        suites += defaultTestLoader.loadTestsFromModule(pyy_doc_tests)
@@ -44,4 +52,5 @@ if tests & VALID['pyy_html']:       suites += defaultTestLoader.loadTestsFromMod
 if tests & VALID['pyy_httpserver']: suites += defaultTestLoader.loadTestsFromModule(pyy_httpserver_tests)
 if tests & VALID['pyy_web']:        suites += defaultTestLoader.loadTestsFromModule(pyy_web_tests)
 
+#Run tests on each module in the array
 TextTestRunner(verbosity=2).run(TestSuite(suites))

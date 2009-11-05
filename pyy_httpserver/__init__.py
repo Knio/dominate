95f6e7f39117b2e87cf54ba1f0a016cfa915a1df
Jake Wharton <JakeWharton@GMail.com>
2009-11-04 21:26:23 -0500

    Fix markdown stupidness.

--- pyy_httpserver/__init__.py
+++ pyy_httpserver/__init__.py
@@ -17,6 +17,7 @@ a list:
     [ SPECIFIER, HANDLER, ... ]
 
 `SPECIFIER` must be one of:
+    
 * A regular expression string - matches that hostname (or uri, in a sub-handler)
 * An HTTP error code - matches that error code
 * `httperror` - matches all errors
@@ -25,13 +26,14 @@ a list:
 
 If no specifiers match, an httperror(404) will be returned.
 
-If multiple SPECIFIERs match, the last one will be used. If the last one
+If multiple specifiers match, the last one will be used. If the last one
 returns a 404 error, then the second last one will be used, etc.
 
 Note that if a leaf-node (final handler) raises a 404, that error will be
 handled normally and will not cause the resolver to keep looking.
 
 `HANDLER` must be one of:
+
 * `[ SPECIFIER, HANDLER, ...]` - to do further processing
 * A function object - will be called with `f(*args)` where `args` is a list of all regex matches that were encountered in the entire processing of this request. The function's return value will then be used as a new `HANDLER`
 * A handler object - will be called with `.handle(httpserver, request, response, *args)` (or `.handle_error(..)` if this is an error match)

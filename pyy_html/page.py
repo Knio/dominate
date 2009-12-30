7b781bd82799d05d1a5733821e50e7086ed49b9d
Tom Flanagan <theknio@gmail.com>
2009-12-29 22:25:18 -0700

    added page class for easy pyy pages

---
+++ pyy_html/page.py
@@ -0,0 +1,45 @@
+from pyy_web import *
+from pyy_html import *
+
+import subprocess
+
+class page(document):
+  css     = []
+  script  = []
+  inline  = False
+
+  def __init__(self, **kwargs):
+    document.__init__(self, **kwargs)
+    for i in self.css:
+      if self.inline:  self.head += style(include(i), type="text/css")
+      else:            self.head += link (href=i,     type="text/css", rel="stylesheet")
+
+    for i in self.script:
+      if self.inline:  self.head += script(include(i), type="text/javascript")
+      else:            self.head += script(href=i,     type="text/javascript")
+
+
+  @classmethod
+  def get(cls, handler ,req, res, *args, **kwargs):
+    p = cls(*args, **kwargs)
+    res.headers['Content-Type'] = 'text/html'
+    res.body = p.render()
+    
+  @classmethod
+  def post(self, handler ,req, res, *args, **kwargs):
+     raise httperror(405)
+    
+    
+  @classmethod
+  def handle(self, handler, req, res, *args, **kwargs):
+    h = getattr(self, req.method.lower(), None)
+    if not h:
+      raise httperror(405)
+    h(handler, req, res, *args, **kwargs)
+    
+    
+      
+    
+    
+
+

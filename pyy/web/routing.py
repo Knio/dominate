d9b843da13e957140535374ae869c0d22686c82c
Jake Wharton <jakewharton@gmail.com>
2011-09-14 00:02:34 -0400

    Ensure function is stored with the routing rule.

--- pyy/web/routing.py
+++ pyy/web/routing.py
@@ -1,18 +1,24 @@
 import re
 
-routes = []
+_routes = []
 
 def get(regex):
-  routes.append(regex_router(regex, 'GET'))
-  return lambda x: x
+  def inner(func):
+    _routes.append(regex_router(func, regex, 'GET'))
+    return func
+  return inner
 
 def post(regex):
-  routes.append(regex_router(regex, 'POST'))
-  return lambda x: x
+  def inner(func):
+    _routes.append(regex_router(func, regex, 'POST'))
+    return func
+  return inner
 
 def directory(root):
-  routes.append(directory_router(root))
-  return lambda x: x
+  def inner(func):
+    _routes.append(directory_router(func, root))
+    return func
+  return inner
 
 
 class router(object):
@@ -20,7 +26,8 @@ class router(object):
     return False
 
 class regex_router(router):
-  def __init__(self, regex, method=None):
+  def __init__(self, func, regex, method=None):
+    self.func = func
     self.regex = re.compile(regex)
     self.method = method
 
@@ -30,14 +37,16 @@ class regex_router(router):
     return False
 
 class directory_router(router):
-  def __init__(self, root):
-    pass
+  def __init__(self, func, root):
+    self.func = func
+    self.root = root
+
 
 def match(uri, method):
-  for route in routes:
+  for route in _routes:
     #TODO handle non-router instances
-    if route.match(uri, method):
-      print('what now?')
+    if route.match(uri, method) and route.func:
+        return route.func
   return _default
 
 

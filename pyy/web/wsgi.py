b6ff075bc2b9fa5b80196297f1758ae5268191ac
Jake Wharton <jakewharton@gmail.com>
2011-09-19 20:35:00 -0400

    Wrap WSGI calls in a simple try/except block for useful output.

--- pyy/web/wsgi.py
+++ pyy/web/wsgi.py
@@ -7,10 +7,25 @@ import routing
 def application(environ, start_response):
   method = environ['REQUEST_METHOD']
   uri = environ['PATH_INFO']
-  body = str(routing.match(uri, method)())
-  #TODO check return value, pass args
 
-  status = '200 OK'
+  try:
+    #TODO check return value, pass args
+    body = str(routing.match(uri, method)())
+    status = '200 OK'
+
+  except SystemExit:
+    pass
+
+  except Exception, e:
+    import traceback
+    body = '''<!DOCTYPE html>
+<html><head><title>Error</title></head><body><pre>
+An error occured while generating this page:
+
+%s
+</pre></body></html>''' % traceback.format_exc()
+    status = '500 INTERNAL SERVER ERROR'
+
   headers = [
     ('Content-Type', 'text/plain'),
     ('Content-Length', str(len(body))),

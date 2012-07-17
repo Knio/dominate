e2193fcfb8c5f8384bceed9bcc0fb9747be36d6d
Tom Flanagan <knio@zkpq.ca>
2012-07-17 00:12:52 -0700

    version++

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ Public License along with pyy.  If not, see
 # http://guide.python-distribute.org/creation.html
 from setuptools import setup, find_packages
 
-version = '1.1.11'
+version = '1.1.12'
 
 
 setup(

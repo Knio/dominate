69c6e3ed2a67e4545e48a6b31b899359515d2bf0
washad <washad@gmail.com>
2019-07-13 13:40:26 -0600

    Fixed underscore not being replaced with dash, and added more svg tags.

--- dominate/svg.py
+++ dominate/svg.py
@@ -4,6 +4,8 @@ This module consists of classes specific to HTML5-SVG Elements. In general this 
 - Elements that are deprecated
 '''
 from dominate.tags import html_tag
+from dominate.dom_tag import dom_tag
+import numbers
 
 
 __license__ = '''
@@ -23,8 +25,7 @@ You should have received a copy of the GNU Lesser General
 Public License along with Dominate.  If not, see
 <http://www.gnu.org/licenses/>.
 '''
-from .dom_tag  import dom_tag, attr
-from .dom1core import dom1core
+
 
 try:
   basestring = basestring
@@ -55,16 +56,36 @@ _ATTR_EVENTS = set([
     'onshow', 'onstalled', 'onsubmit', 'onsuspend', 'ontimeupdate', 'ontoggle', 'onvolumechange', 'onwaiting'
 ])
 
+DASHED_ATTRIBUTES = set([
+    'accent', 'arabic', 'baseline', 'cap', 'clip', 'color', 'enable', 'fill', 'flood', 'font', 'glyph',
+    'horiz', 'image', 'letter', 'lighting', 'marker', 'panose', 'paint', 'pointer', 'shape', 'stop',
+    'stroke', 'text', 'unicode', 'units', 'v', 'vert', 'x'
+])
+
 
 # https://developer.mozilla.org/en-US/docs/Web/SVG/Element/svg
 
 # SVG
 
-class svg(html_tag):
+class svg_tag(html_tag):
+
+    def __init__(self, *args, **kwargs):
+
+        # Replace '_' with '-' for svg elements that use dashed.
+        for k in [k for k in kwargs.items() if isinstance(k, tuple)]:
+            key = k[0]
+            if '_' in key and key.split('_')[0] in DASHED_ATTRIBUTES:
+                r = k[0].replace('_', '-')
+                kwargs[r] = kwargs.pop(k[0])
+        super().__init__(args, kwargs)
+
+
+class svg(svg_tag):
     pass
 
 
-class animate(html_tag):
+
+class animate(svg_tag):
     '''
     The  animate SVG element is used to animate an attribute or property of an element over time.
     It's normally inserted inside the element or referenced by the href attribute of the target element.
@@ -72,14 +93,14 @@ class animate(html_tag):
     pass
 
 
-class animateMotion(html_tag):
+class animateMotion(svg_tag):
     '''
     The <animateMotion> element causes a referenced element to move along a motion path.
     '''
     pass
 
 
-class animateTransform(html_tag):
+class animateTransform(svg_tag):
     '''
     The animateTransform element animates a transformation attribute on its target element, thereby allowing
     animations to control translation, scaling, rotation, and/or skewing.
@@ -87,21 +108,21 @@ class animateTransform(html_tag):
     is_single = True
 
 
-class circle(html_tag):
+class circle(svg_tag):
     '''
     The <circle> SVG element is an SVG basic shape, used to draw circles based on a center point and a radius.
     '''
     pass
 
 
-class clipPath(html_tag):
+class clipPath(svg_tag):
     '''
     The <clipPath> SVG element defines a clipping path, to be used used by the clip-path property.
     '''
     pass
 
 
-class defs(html_tag):
+class defs(svg_tag):
     '''
     The <defs> element is used to store graphical objects that will be used at a later time. Objects created inside a
     <defs> element are not rendered directly. To display them you have to reference them
@@ -110,21 +131,59 @@ class defs(html_tag):
     pass
 
 
-class desc(html_tag):
+class desc(svg_tag):
     '''
     The <desc> element provides an accessible, long-text description of any SVG container element or graphics element.
     '''
     pass
 
 
-class ellipse(html_tag):
+class ellipse(svg_tag):
     '''
     An ellipse element for svg containers
     '''
     pass
 
+# (Note, filters are at the bottom of this file)
 
-class mpath(html_tag):
+class g(svg_tag):
+    '''
+    The <g> SVG element is a container used to group other SVG elements.
+    '''
+    pass
+
+
+class image(svg_tag):
+    '''
+    The <image> SVG element includes images inside SVG documents. It can display raster image files or other SVG files.
+    '''
+    pass
+
+
+class line(svg_tag):
+    '''
+    The <line> element is an SVG basic shape used to create a line connecting two points.
+    '''
+    pass
+
+
+class linearGradient(svg_tag):
+    '''
+    The <linearGradient> element lets authors define linear gradients that can be applied to fill or
+    stroke of graphical elements.
+    '''
+    pass
+
+
+class marker(svg_tag):
+    '''
+    The <marker> element defines the graphic that is to be used for drawing arrowheads or polymarkers on a given <path>, <line>, <polyline> or <polygon> element.
+    '''
+    pass
+
+
+
+class mpath(svg_tag):
     '''
     The <mpath> sub-element for the <animateMotion> element provides the ability to reference an
     external <path> element as the definition of a motion path.
@@ -132,7 +191,7 @@ class mpath(html_tag):
     pass
 
 
-class text(html_tag):
+class text(svg_tag):
     '''
     The SVG <text> element draws a graphics element consisting of text. It's possible to apply a gradient,
      pattern, clipping path, mask, or filter to <text>, like any other SVG graphics element.
@@ -140,7 +199,7 @@ class text(html_tag):
     pass
 
 
-class textPath(html_tag):
+class textPath(svg_tag):
     '''
     To render text along the shape of a <path>, enclose the text in a <textPath> element that has an href
     attribute with a reference to the <path> element.
@@ -148,7 +207,7 @@ class textPath(html_tag):
     pass
 
 
-class tspan(html_tag):
+class tspan(svg_tag):
     '''
     The SVG <tspan> element define a subtext within a <text> element or another <tspan> element.
     It allows to adjust the style and/or position of that subtext as needed.
@@ -156,46 +215,107 @@ class tspan(html_tag):
     pass
 
 
-class line(html_tag):
-  '''
-  A line element for svg containers
-  '''
-  pass
+class path(svg_tag):
+    '''
+    A path element for svg containers
+    '''
+    pass
 
-class linearGradient(html_tag):
-  '''
-  Linear gradient element for svg containers
-  '''
-  pass
+class polygon(svg_tag):
+    '''
+    A polygon element for svg containers
+    '''
+    pass
 
-class path(html_tag):
-  '''
-  A path element for svg containers
-  '''
-  pass
+class polyline(svg_tag):
+    '''
+    A polyline element for svg containers
+    '''
+    pass
 
-class polygon(html_tag):
-  '''
-  A polygon element for svg containers
-  '''
-  pass
+class rect(svg_tag):
+    '''
+    A rectangle element for svg containers
+    '''
+    pass
 
-class polyline(html_tag):
-  '''
-  A polyline element for svg containers
-  '''
-  pass
+class stop(svg_tag):
+    '''
+    The SVG <stop> element defines a color and its position to use on a gradient.
+    This element is always a child of a <linearGradient> or <radialGradient> element.
+    '''
+    pass
 
-class rect(html_tag):
-  '''
-  A rectangle element for svg containers
-  '''
-  pass
-
-class use(html_tag):
+class use(svg_tag):
     '''
     The <use> element takes nodes from within the SVG document, and duplicates them somewhere else.
     '''
     pass
+
+
+
+# FILTERS
+class filter(svg_tag):
+    pass
+
+class feBlend(svg_tag):
+    pass
+
+class feColorMatrix(svg_tag):
+    pass
+
+class feComponentTransfer(svg_tag):
+    pass
+
+class feComposite(svg_tag):
+    pass
+
+class feConvolveMatrix(svg_tag):
+    pass
+
+class feDiffuseLighting(svg_tag):
+    pass
+
+class feDisplacementMap(svg_tag):
+    pass
+
+class feFlood(svg_tag):
+    pass
+
+class feGaussianBlur(svg_tag):
+    pass
+
+class feImage(svg_tag):
+    pass
+
+class feMerge(svg_tag):
+    pass
+
+class feMorphology(svg_tag):
+    pass
+
+class feOffset(svg_tag):
+    pass
+
+class feSpecularLighting(svg_tag):
+    pass
+
+class feTile(svg_tag):
+    pass
+
+class feTurbulence(svg_tag):
+    pass
+
+class feDistantLight(svg_tag):
+    pass
+
+class fePointLight(svg_tag):
+    pass
+
+class feSpotLight(svg_tag):
+    pass
+
+
+
 
 
--- tests/test_svg.py
+++ tests/test_svg.py
@@ -2,6 +2,7 @@ from dominate.tags import *
 from dominate.svg import *
 from htmldiffer import utils as diff_utils
 import pytest
+from dominate import svg as m_svg
 
 
 def html_equals(html1, html2):
@@ -12,16 +13,21 @@ def html_equals(html1, html2):
     diffs = [(line[0], line[1]) for line in zip(lines1, lines2) if line[0] != line[1]]
     return lines1 == lines2
 
+
 def base():
     return svg(width=120, height=120, viewBox="0 0 120 120", version="1.1",
               xmlns="http://www.w3.org/2000/svg", xmlns_xlink="http://www.w3.org/1999/xlink")
 
 
 def make_circle(cx: int = 50, cy: int=60, r: int=30, fill: str = "black"):
-    return circle(cx=str(cx), cy=str(cy), r=str(r), fill=fill)
+    if fill:
+        return circle(cx=str(cx), cy=str(cy), r=str(r), fill=fill)
+    else:
+        return circle(cx=str(cx), cy=str(cy), r=str(r))
 
 
 # Note, all tests produce working examples. The expected results can be pasted into https://jsfiddle.net/api/mdn/
+
 
 
 def test_animate():
@@ -42,7 +48,7 @@ def test_animate():
 def test_animate_motion():
     expected = """
     <svg height="120" version="1.1" viewBox="0 0 120 120" width="120" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <path d="M10,110 A120,120 -45 0,1 110 10 A120,120 -45 0,1 10,110" fill="none" id="theMotionPath" stroke="lightgrey" stroke_width="2"></path>
+      <path d="M10,110 A120,120 -45 0,1 110 10 A120,120 -45 0,1 10,110" fill="none" id="theMotionPath" stroke="lightgrey" stroke-width="2"></path>
       <circle cx="10" cy="110" fill="lightgrey" r="3"></circle>
       <circle cx="110" cy="10" fill="lightgrey" r="3"></circle>
       <circle cx="" cy="" fill="red" r="5">
@@ -100,7 +106,7 @@ def test_clip_path():
         <circle cx="20" cy="30" r="40"></circle>
       </clipPath>
       <path d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z" id="heart"></path>
-      <use clip_path="url(#MyClip)" fill="red" xlink:href="#heart"></use>
+      <use clip-path="url(#MyClip)" fill="red" xlink:href="#heart"></use>
     </svg>
     """
     with base() as result:
@@ -137,6 +143,99 @@ def test_ellipse():
     with base() as result:
         ellipse(cx="100", cy="50", rx="100", ry="50" )
     assert html_equals(result.render(), expected)
+
+
+filter_names = ['feBlend', 'feColorMatrix', 'feComponentTransfer', 'feComposite', 'feConvolveMatrix', 'feDiffuseLighting',
+           'feDisplacementMap', 'feFlood', 'feGaussianBlur', 'feImage', 'feMerge', 'feMorphology', 'feOffset',
+           'feSpecularLighting', 'feTile', 'feTurbulence', 'feDistantLight', 'fePointLight', 'feSpotLight']
+
+
+def test_filters():
+    for name in filter_names:
+        attr = getattr(m_svg, name)
+        with filter() as f:
+            attr()
+        expected = f"""
+            <filter>
+                <{name}></{name}>
+            </filter>
+        """
+        assert html_equals(f.render(), expected)
+
+def test_g():
+    expected = """
+    <svg height="120" version="1.1" viewBox="0 0 120 120" width="120" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <g fill="white" stroke="green" stroke-width="5">
+        <circle cx="40" cy="40" r="25"></circle>
+        <circle cx="60" cy="60" r="25"></circle>
+      </g>
+    </svg>
+    """
+    with base() as result:
+        with g(fill="white", stroke="green", stroke_width="5"):
+            make_circle(40, 40, 25, None)
+            make_circle(60, 60, 25, None)
+    assert html_equals(result.render(), expected)
+
+
+def test_line():
+    expected = """
+    <svg height="120" version="1.1" viewBox="0 0 120 120" width="120" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <line stroke="red" x0="0" x1="50" y0="0" y1="50"></line>
+    </svg>
+    """
+    with base() as result:
+        line(x0='0', x1='50', y0='0', y1='50', stroke='red')
+    assert html_equals(result.render(), expected)
+
+
+def test_linear_gradient():
+    expected = """
+    <svg height="120" version="1.1" viewBox="0 0 120 120" width="120" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <defs>
+            <linearGradient gradientTransform="rotate(90)" id="myGradient">
+              <stop offset="5%" stop-color="gold"></stop>
+              <stop offset="95%" stop-color="red"></stop>
+            </linearGradient>
+        </defs>
+        <circle cx="50" cy="50" fill="url('#myGradient')" r="40"></circle>
+    </svg>
+    """
+    with base() as result:
+        with defs():
+            with linearGradient(id="myGradient", gradientTransform="rotate(90)"):
+                stop(offset="5%", stop_color="gold")
+                stop(offset="95%", stop_color="red")
+
+        make_circle(50, 50, 40, "url('#myGradient')")
+
+    assert html_equals(result.render(), expected)
+
+
+def test_marker():
+    expected = """
+    <svg height="120" version="1.1" viewBox="0 0 120 120" width="120" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <defs>
+        <marker id="arrow" markerHeight="6" markerWidth="6" orient="auto-start-reverse" refX="5" refY="5" viewBox="0 0 10 10">
+          <path d="M 0 0 L 10 5 L 0 10 z"></path>
+        </marker>
+      </defs>
+      <polyline fill="none" marker-end="url(#arrow)" marker-start="url(#arrow)" points="10,10 10,90 90,90" stroke="black"></polyline>
+    </svg>"""
+    with base() as result:
+        with defs():
+            with marker(id="arrow", viewBox="0 0 10 10", refX="5", refY="5",
+                        markerWidth="6", markerHeight="6", orient="auto-start-reverse"):
+                path(d="M 0 0 L 10 5 L 0 10 z")
+
+        polyline(points="10,10 10,90 90,90", fill="none", stroke="black", marker_start="url(#arrow)",
+                      marker_end="url(#arrow)" )
+
+    assert html_equals(result.render(), expected)
+
+
+
+
 
 
 

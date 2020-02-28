030cc58ebe809911c35fd540559d7047497ce9bd
Tom Flanagan <tom@zkpq.ca>
2020-02-27 21:33:43 -0800

    Update CI, versions

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: Implementation :: PyPy',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development :: Libraries :: Python Modules',

9cf8ce133e7356c9f26da083201df230d6027cd7
Nikalexis Nikos <nikalexis@gmail.com>
2024-08-28 17:06:40 +0300

    Fix include

--- setup/setup.py
+++ setup/setup.py
@@ -17,7 +17,7 @@ Public License along with dominate.  If not, see
 '''
 # pylint: disable=bad-whitespace
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 import imp
 _version = imp.load_source("dominate._version", "dominate/_version.py")
@@ -60,6 +60,6 @@ setup(
   install_requires=[
     'cssutils >= 2.11.1',
   ],
-  packages = ['dominate*'],
+  packages = find_packages(include=['dominate', 'dominate.*']),
   include_package_data = True,
 )
